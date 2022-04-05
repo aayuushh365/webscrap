55c5f3abbdb78a5eebab88f13cc8ee702d28cbdc
Aayush Singh <74130928+aayuushh365@users.noreply.github.com>
2022-04-05 12:39:49 +0530

    Update emailautomation.py

--- emailautomation.py
+++ emailautomation.py
@@ -12,12 +12,11 @@ content = ''  # email content placeholder
 
 # extracting hacker news stories
 
-def extract_news(url):  # creating a function to extract news
-    print('Extracting Hacker news Stories...')  # to keep user updated printing this message
-    cnt = ''  # creating temporary placeholder. this holder is used to assign value to content(line 12)
-    cnt += ('<b> CN top Stories: </b>\n' + '<br>' + '-' * 50 + '<br>')    # <b>- for bolding the statement <br>- break
-    response = requests.get(url)   # using get function of the request package to get the content of the url and then store it in the response object
-    # The content once we get from the get function is actually in  response body HTTP response, which will contain content the actual content that is required for  us which is the content of the webpage.
+def extract_news(url):  
+    print('Extracting Hacker news Stories...')  
+    cnt = ''  #  temporary placeholder. this holder is used to assign value to content
+    cnt += ('<b> CN top Stories: </b>\n' + '<br>' + '-' * 50 + '<br>') 
+    response = requests.get(url)  
     content = response.content # here content is the scope in extract_news. so content in line 12 is different from content in line 23
     soup = BeautifulSoup(content,'html.parser')         # here html.parser is used to extract the soup out of content in line 23 from where we got the response
 
@@ -26,24 +25,21 @@ def extract_news(url):  # creating a function to extract news
     return cnt
 cnt = extract_news('https://https://news.ycombinator.com/')
 content = content+cnt
-content = content + ('<br>------------</br>')                # putting br at the end of the content
+content = content + ('<br>------------</br>')             
 content = content + ('<br></br>End of Message!')
 
 
-# sending the email
-
 print('Composing Email...')
 
-# updating your email details
+
 SERVER = 'smtp.gmail.com'   # your smtp server
-PORT = 587 # your port number i.e gmail port no is 587
-FROM = ''    # from where you need to send the email
-TO = ''     # your email address #can be a python list
-PASS = ''  # your email id password
+PORT = 587                  # your port number i.e gmail port no is 587
+FROM = ''                   # from where you need to send the email
+TO = ''                     # your email address #can be a python list
+PASS = ''                   # your email id password
 
 # creating a message body
 msg = MIMEMultipart()
-#creating email subject line
 msg['Subject'] = 'Top News Stories from HN [Automated Email]' + '' + str(now.day) + '-' + str(now.year)
 msg['From']  = FROM
 msg['To'] = TO
